bc61894a15e435db8b6fe8e2d1d797db34e98c1f
minas <minas@192.168.0.11>
2020-07-29 17:47:56 -0400

    I committed it again.

--- BabySitterProjectOHealth/src/com/babySitter/BabysitterKataOH.java
+++ BabySitterProjectOHealth/src/com/babySitter/BabysitterKataOH.java
@@ -3,7 +3,7 @@ package com.babySitter;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 public class BabysitterKataOH {
-	static double total=0;
+	static double totalPayment=0.0;
 
 	public static void main(String[] args) {
 
@@ -11,39 +11,28 @@ public class BabysitterKataOH {
 	}
 
 	public static double extractedLocalTime() {
-         LocalDateTime startTimeNow = LocalDateTime.of(2020, 07, 26, 18, 00, 00);  
-         LocalDateTime checkIn = LocalDateTime.of(2020, 07, 26, 17, 00, 00);  
-         LocalDateTime bedTime = LocalDateTime.of(2020, 07, 26, 22, 00, 00);  
-         LocalDateTime midNighTime = LocalDateTime.of(2020, 07, 27, 00, 00, 00); 
-         LocalDateTime checkOut = LocalDateTime.of(2020, 07, 27, 04, 00, 00);  
-
-
-      
-
+		LocalDateTime startTimeNow = LocalDateTime.of(2020, 07, 27, 04, 00, 00);  
+		LocalDateTime checkIn = LocalDateTime.of(2020, 07, 26, 17, 00, 00);  
+		LocalDateTime bedTime = LocalDateTime.of(2020, 07, 26, 22, 00, 00);  
+		LocalDateTime midNighTime = LocalDateTime.of(2020, 07, 27, 00, 00, 00); 
+		LocalDateTime checkOut = LocalDateTime.of(2020, 07, 27, 04, 00, 00);  
 		if(startTimeNow.isAfter(checkIn) ||(startTimeNow.isBefore(checkOut))) {
-			
-		if(startTimeNow.isAfter(checkIn) ||(startTimeNow.isBefore(bedTime)) ) {
-			total=total +((ChronoUnit.HOURS.between(checkIn, bedTime))*8); 
-			
-		  }
-		
-		if(startTimeNow.isAfter(bedTime) ||(startTimeNow.isBefore(midNighTime)) ) {
-			total=total +((ChronoUnit.HOURS.between(checkIn, bedTime))*12); 
-			
-		  }
-		if(startTimeNow.isAfter(midNighTime) ||(startTimeNow.isBefore(checkOut)) ) {
-			total=total +((ChronoUnit.HOURS.between(checkIn, bedTime))*16); 
-		}
-		 
-			
-			 } else { System.out.
-			  println("Starts no earlier than 5:00 PM or leaves no later than 4:00 AM");
-			  
-			  }
-			
 
-		
-		return total;
+			if(startTimeNow.isAfter(checkIn) ||(startTimeNow.isBefore(bedTime)) ) {
+				totalPayment=totalPayment +((ChronoUnit.HOURS.between(checkIn, bedTime))*8); 
+			}
+			if(startTimeNow.isAfter(bedTime) ||(startTimeNow.isBefore(midNighTime)) ) {
+				totalPayment=totalPayment +((ChronoUnit.HOURS.between(checkIn, bedTime))*12); 
+			}
+			if(startTimeNow.isAfter(midNighTime) ||(startTimeNow.isBefore(checkOut)) ) {
+				totalPayment=totalPayment +((ChronoUnit.HOURS.between(checkIn, bedTime))*16);
+			}
+		} else { System.out.
+			println("Starts no earlier than 5:00 PM or leaves no later than 4:00 AM");
+
+		}
+
+		return totalPayment;
 
 	}
 
